0dee092bc0122821086b911c5c1387e33146aa44
Raquel212 <raquelcristinamartins750@gmail.com>
2024-12-20 16:27:55 -0300

    Adciona configuração inicial do entity framework

---
+++ MediCareApi/Repository/Context/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using MediCareApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediCareApi.Repository.Context;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Pessoa> Pessoas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql(
+            "Server=localhost " +
+            "Port=5432;" +
+            "Database=postgres;" +
+            "Uid=admin;" +
+            "Pwd=admin;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        
+    }
+}
