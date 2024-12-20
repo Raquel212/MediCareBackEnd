857d0e87064e985015f2a30d667fbf3bed169937
Raquel212 <raquelcristinamartins750@gmail.com>
2024-12-20 16:33:12 -0300

    Cria migrations iniciais

---
+++ MediCareApi/Migrations/20241220193234_initial.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediCareApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "MediCare");
+
+            migrationBuilder.CreateTable(
+                name: "Pessoa",
+                schema: "MediCare",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Nome = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Sobrenome = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoa", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                schema: "MediCare",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Email = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Senha = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    PessoaId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Usuario_Pessoa_PessoaId",
+                        column: x => x.PessoaId,
+                        principalSchema: "MediCare",
+                        principalTable: "Pessoa",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuario_PessoaId",
+                schema: "MediCare",
+                table: "Usuario",
+                column: "PessoaId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuario",
+                schema: "MediCare");
+
+            migrationBuilder.DropTable(
+                name: "Pessoa",
+                schema: "MediCare");
+        }
+    }
+}
