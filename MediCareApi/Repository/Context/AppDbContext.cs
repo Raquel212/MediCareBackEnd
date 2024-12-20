70deed90fedfa900cd6f7ee027824d735ffa2e4a
Raquel212 <raquelcristinamartins750@gmail.com>
2024-12-20 16:36:23 -0300

    Corrige connection string do AppDbContext

--- MediCareApi/Repository/Context/AppDbContext.cs
+++ MediCareApi/Repository/Context/AppDbContext.cs
@@ -11,7 +11,7 @@ public class AppDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder.UseNpgsql(
-            "Server=localhost " +
+            "Server=localhost;" +
             "Port=5432;" +
             "Database=postgres;" +
             "Uid=admin;" +
