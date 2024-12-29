2c2f14c9bdf867fafdc7b4bdfca74e5e28e86ed7
Raquel212 <raquelcristinamartins750@gmail.com>
2024-12-29 09:41:57 -0300

    Torna coluna horario nullable em medicamento

---
+++ MediCareApi/Migrations/20241229124135_ColunaNullableMedicamento.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediCareApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class ColunaNullableMedicamento : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Horario",
+                schema: "MediCare",
+                table: "Medicamento",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Horario",
+                schema: "MediCare",
+                table: "Medicamento",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
