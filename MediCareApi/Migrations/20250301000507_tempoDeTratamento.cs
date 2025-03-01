08ea9bc6eece14ff77766cfc20c1cdf31de1e1ce
Raquel212 <101139920+Raquel212@users.noreply.github.com>
2025-02-28 21:06:00 -0300

    migracao do tempo de tramento do medicamento

---
+++ MediCareApi/Migrations/20250301000507_tempoDeTratamento.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediCareApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class tempoDeTratamento : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TempoDeTratamento",
+                schema: "MediCare",
+                table: "Medicamento",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TempoDeTratamento",
+                schema: "MediCare",
+                table: "Medicamento");
+        }
+    }
+}
